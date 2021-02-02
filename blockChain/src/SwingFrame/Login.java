cd62e56f7520cf63967da6267dc9b5aff32190e8
XDwan <xdwanshijie@outlook.com>
2021-02-02 15:06:09 +0800

    Login部分修改

--- blockChain/src/SwingFrame/Login.java
+++ blockChain/src/SwingFrame/Login.java
@@ -1,5 +1,8 @@
 package SwingFrame;
 
+import Message.MessageType;
+import net.sf.json.JSONObject;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -9,6 +12,7 @@ public class Login extends JFrame {
     String account="";
     String password="";
     String loginType="";
+    JSONObject object = new JSONObject();
 
     public Login(){
         this.setTitle("登陆界面");
@@ -16,23 +20,23 @@ public class Login extends JFrame {
         this.setSize(500,400);
         Container container = this.getContentPane();
 //        登陆账号部分
-        JLabel label_Account = new JLabel("账号");
+        JLabel label_Account = new JLabel("学号");
         JLabel label_Password = new JLabel("密码");
         label_Account.setFont(new Font("",0,22));
         label_Password.setFont(new Font("",0,22));
         container.add(label_Account);
         container.add(label_Password);
-        label_Account.setBounds(50,50,50,50);
-        label_Password.setBounds(50,100,50,50);
-        JTextField account = new JTextField();
-        JPasswordField password = new JPasswordField();
-        account.setBounds(100,50,150,35);
-        password.setBounds(100,100,150,35);
-        container.add(account);
-        container.add(password);
-        account.setFont(new Font("",0,22));
-        password.setFont(new Font("",0,22));
-        password.setEchoChar('*');
+        label_Account.setBounds(75,40,50,50);
+        label_Password.setBounds(75,90,50,50);
+        JTextField account_input = new JTextField();
+        JPasswordField password_input = new JPasswordField();
+        account_input.setBounds(150,50,225,35);
+        password_input.setBounds(150,100,225,35);
+        container.add(account_input);
+        container.add(password_input);
+        account_input.setFont(new Font("",0,22));
+        password_input.setFont(new Font("",0,22));
+        password_input.setEchoChar('*');
 
 //        单选框
         ButtonGroup buttonGroup = new ButtonGroup();
@@ -40,10 +44,12 @@ public class Login extends JFrame {
         JRadioButton radioButton_teacher = new JRadioButton("教师");
         buttonGroup.add(radioButton_student);
         buttonGroup.add(radioButton_teacher);
+        radioButton_student.setBounds(150,200,75,50);
+        radioButton_teacher.setBounds(275,200,75,50);
+        radioButton_student.setFont(new Font("",0,22));
+        radioButton_teacher.setFont(new Font("",0,22));
         buttonGroup.setSelected( radioButton_student.getModel(),true);
-
         loginType = "student";
-
         radioButton_student.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -58,13 +64,31 @@ public class Login extends JFrame {
                 System.out.println(loginType);
             }
         });
-
-
+        JButton button_login = new JButton("登陆");
+        button_login.setFont(new Font("",0,22));
+        button_login.setBounds(150,280,200,50);
+        button_login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                account = account_input.getText();
+                password = new String(password_input.getPassword());
+                object.put("account",account);
+                object.put("password",password);
+                if (loginType.compareTo("student")==0) object.put("MessageTpye", MessageType.Stu_Login);
+                else object.put("MessageTpye", MessageType.Teacher_Login);
+                System.out.println(object);
+            }
+        });
+        container.add(button_login);
         container.add(radioButton_student);
         container.add(radioButton_teacher);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
+    }
+
+    public JSONObject getObject() {
+        return object;
     }
 
     public static void main(String[] args) {
