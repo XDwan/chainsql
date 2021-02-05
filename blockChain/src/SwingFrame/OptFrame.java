699aa4fc020c238d18d54e590c5b5f05c7229c8e
XDwan <xdwanshijie@outlook.com>
2021-02-05 11:05:58 +0800

    主界面部分更新

--- blockChain/src/SwingFrame/OptFrame.java
+++ blockChain/src/SwingFrame/OptFrame.java
@@ -5,10 +5,12 @@ import messageCollection.MessageType;
 import net.sf.json.JSONObject;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class OptFrame extends JFrame implements Runnable {
     JSONObject account = new JSONObject();
-
+    Container container = this.getContentPane();
+    JPanel statusStu = new JPanel();
     public OptFrame(Message reMessage){
         account = reMessage.getObject();
     }
@@ -18,7 +20,34 @@ public class OptFrame extends JFrame implements Runnable {
 
     }
 
-    public void frameShow(){
+    public void frameShowStu(){
+        this.setTitle("学生考勤界面");
+        this.setSize(600,500);
 
+        // 签到按钮
+
+        // 状态面板
+        statusStu.setBounds(0,0,200,200);
+        container.add(statusStu);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public void frameShowTeacher(){
+        this.setTitle("教师考勤管理界面");
+    }
+
+    public void setStuState(JSONObject state){
+        // 状态标签
+        JLabel name = new JLabel("姓名："+state.getString("Name"));
+        JLabel id = new JLabel("学号："+state.getString("id"));
+        JLabel status = new JLabel("当前状态："+state.getString("Status"));
+        JLabel signInStatus = new JLabel("签到状态"+state.getString("SignInStatus"));
+
+
+    }
+
+    public static void main(String[] args) {
+        new OptFrame(new Message()).frameShowStu();
     }
 }
